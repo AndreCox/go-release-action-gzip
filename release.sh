e5274d29821de6acfd1d348cb0bed30c3276fe2c
Andre <arc.xoc@gmail.com>
2022-07-30 18:37:32 -0700

    Update release.sh

--- release.sh
+++ release.sh
@@ -110,8 +110,8 @@ if [ ${INPUT_COMPRESS_ASSETS^^} == 'TRUE' ]; then
   MEDIA_TYPE='application/gzip'
   RELEASE_ASSET_FILE=${RELEASE_ASSET_NAME}${RELEASE_ASSET_EXT}
   if [ ${INPUT_GOOS} == 'windows' ]; then
-    RELEASE_ASSET_EXT='.zip'
-    MEDIA_TYPE='application/zip'
+    RELEASE_ASSET_EXT='.tar.gz'
+    MEDIA_TYPE='application/gzip'
     RELEASE_ASSET_FILE=${RELEASE_ASSET_NAME}${RELEASE_ASSET_EXT}
     ( shopt -s dotglob; zip -vr ${RELEASE_ASSET_FILE} * )
   else
